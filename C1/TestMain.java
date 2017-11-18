fc770b98863470e6d38e0247ffe03d72c323137a
icruise <shopping.info@gmail.com>
2017-11-17 19:49:44 -0800

    Add first file

---
+++ C1/TestMain.java
@@ -0,0 +1,9 @@
+
+
+public class TestMain {
+    public static void  main(String []args) {
+        //System.out.print("Hello");
+        int a = (int)12.5 % 3;
+        System.out.format("%d", a);
+    }
+}

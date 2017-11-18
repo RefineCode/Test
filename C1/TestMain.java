4f8519d7c1a4e86587b3f0ed2597b406bc43f5c9
icruise <shopping.info@gmail.com>
2017-11-17 19:58:52 -0800

    add a line

--- C1/TestMain.java
+++ C1/TestMain.java
@@ -2,7 +2,7 @@
 
 public class TestMain {
     public static void  main(String []args) {
-        //System.out.print("Hello");
+        System.out.print("Hello");
         int a = (int)12.5 % 3;
         System.out.format("%d", a);
     }
